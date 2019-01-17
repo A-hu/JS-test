3790e95a63ddf7ed12865b4d0db63c005d5786dd
A-hu <louis0911317399@gmail.com>
2019-01-18 01:45:51 +0800

    exercise1 refined

--- tests/exercise1.test.js
+++ tests/exercise1.test.js
@@ -1,31 +1,36 @@
 const exercise1 = require('../exercise1');
 
 describe('FizzBuzz', () => {
-  it('should show FizzBuzz in 15', () => {
+  // it('should show FizzBuzz in 15', () => {
+  it('should return FizzBuzz if input is divisible by 3 and 5', () => {
     const result = exercise1.fizzBuzz(15);
 
     expect(result).toBe('FizzBuzz');
   });
 
-  it('should show Fizz in 3', () => {
+  it('should return Fizz if input is divisible by 3', () => {
     const result = exercise1.fizzBuzz(3);
 
     expect(result).toBe('Fizz');
   });
 
-  it('should show Buzz in 5', () => {
+  it('should return Buzz if input is divisible by 5', () => {
     const result = exercise1.fizzBuzz(5);
 
     expect(result).toBe('Buzz');
   });
 
-  it('should show original input in 7', () => {
-    const result = exercise1.fizzBuzz(7);
+  it('should return input if it is not divisible by 3 or 5', () => {
+    const result = exercise1.fizzBuzz(1);
 
-    expect(result).toBe(7);
+    expect(result).toBe(1);
   });
 
-  it('should throw error without number', () => {
+  // it('should throw error without number', () => {
+  it('should throw an exception if input is not a number', () => {
     expect(() => { exercise1.fizzBuzz('Hello') }).toThrow();
+    expect(() => { exercise1.fizzBuzz(null) }).toThrow();
+    expect(() => { exercise1.fizzBuzz(undefined) }).toThrow();
+    expect(() => { exercise1.fizzBuzz({}) }).toThrow();
   });
 });
