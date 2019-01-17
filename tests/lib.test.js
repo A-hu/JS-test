675dd564cb5e11b689dde13e321117aa19c32610
A-hu <louis0911317399@gmail.com>
2019-01-18 00:45:19 +0800

    Testing Exceptions

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -60,3 +60,19 @@ describe('getProduct', () => {
     expect(result).toHaveProperty('price', 10);
   });
 });
+
+describe('registerUser', () => {
+  it('should throw if username is falsy', () => {
+    const args = [null, undefined, NaN, '', 0, false];
+    args.forEach(a => {
+      expect(() => { lib.registerUser(a) }).toThrow();
+    });
+  });
+
+  it('should return a user object if valid username is passed', () => {
+    const result = lib.registerUser('Louis');
+
+    expect(result).toMatchObject({ username: 'Louis' });
+    expect(result.id).toBeGreaterThan(0);
+  });
+});
